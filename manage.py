257eb9d76529a4ee6a0ed10ea0d6f438c1f11228
raichominev <raicho@mail.bg>
2020-03-12 04:16:53 +0200

    .

--- manage.py
+++ manage.py
@@ -2,7 +2,7 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
-
+import proxy
 
 def main():
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'photomgr.settings')
@@ -18,4 +18,7 @@ def main():
 
 
 if __name__ == '__main__':
-    main()
+
+    with proxy.start([]):
+        main()
+
